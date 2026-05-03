985b62b76fb6daae391193e8ca6307c139d054fb
Shoumik Ghosh <pantumdelux@gmail.com>
2026-05-03 11:32:22 +1000

    Adding comments

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Scanner is used to read input from the keyboard.
         Scanner input = new Scanner(System.in);
+
+        // Create one Game object to control the whole match.
         Game game = new Game();
 
+        // Start the game.
         game.play(input);
 
+        // Close Scanner after the game is finished.
         input.close();
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,23 @@
 public class Player {
+
+    // Stores the player's name
     String name;
+
+    // Stores the player's score
     int score;
 
+    /*
+     * Constructor: creates a new Player object
+     * and sets initial score to 0
+     */
     public Player(String name) {
         this.name = name;
         score = 0;
     }
 
+    /*
+     * Adds 1 point to the player's score
+     */
     public void addPoint() {
         score++;
     }
